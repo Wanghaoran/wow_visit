e2fcbab98f6a4b4829bf81f127ab1e05f32ad802
WangHaoRan <whrlmc@139.com>
2014-11-05 09:31:10 +0800

    23123

---
+++ application/views/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=7" />
+    <title>艾泽拉斯旅游局</title>
+    <link href="<?=$this->config->base_url()?>/static/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="<?=$this->config->base_url()?>/static/css/album.css" rel="stylesheet" type="text/css" media="all" />
+
+</head>
+<body>
+<div class="contain">
+    <!--header start-->
+    <div class="header">
+        <a href="#" class="logo"></a>
+        <div class="clear"></div>
+        <div class="shipin">
+            <embed src="http://player.youku.com/player.php/sid/XODAzNTkwMTQ0/v.swf" quality="high" width="493" height="323" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash"></embed>
+        </div>
+
+    </div>
+    <!--header end-->
+    <!--star start-->
+    <div class="zkmx"><img src="<?=$this->config->base_url()?>/static/images/img_zkmx.jpg"/></div>
+    <!--star end-->
+    <!--album start-->
+    <div class="xcbox">
+        <div class="selectbox">
+            <select class="select_area"><option></option><option>请选择</option></select>
+
+        </div>
+        <ul class="tab">
+            <li class="nocur"><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+        <div class="clear"></div>
+        <div class="album" id="album">
+            <div class="album-image-md" id="album-image-md">
+                <p class="album-image-bd"><img src="<?=$this->config->base_url()?>/static/images/01.jpg" class="album-image" id="album-image" alt="" width="487" height="300" /></p>
+                <p class="album-image-ft" id="album-image-ft">相册图片-示例图片（1）</p>
+                <!--<a class="album-download" id="album-download" href="images/01.jpg" target="_blank"></a>-->
+                <ul class="album-image-nav hide" id="album-image-nav">
+                    <li class="album-image-nav-left-block" id="album-image-nav-left-block"><a href="#prev-image" class="album-image-btn-prev" id="album-image-btn-prev"></a></li>
+                    <li class="album-image-nav-right-block" id="album-image-nav-right-block"><a href="#next-image" class="album-image-btn-next" id="album-image-btn-next"></a></li>
+                </ul>
+                <p class="album-image-loading-overlay hide" id="album-image-loading-overlay"><img src="<?=$this->config->base_url()?>/static/images/loading.gif" alt="loading..." width="100" height="100" /></p>
+            </div>
+            <div class="album-carousel" id="album-carousel">
+                <!--<a href="#prev-group" class="album-carousel-btn-prev" id="album-carousel-btn-prev">‹</a>-->
+                <div class="album-carousel-zone" id="album-carousel-zone">
+                    <ul class="album-carousel-list" id="album-carousel-list">
+                        <li class="album-carousel-thumb li1 album-carousel-thumb-selected"><a href="<?=$this->config->base_url()?>/static/images/01.jpg"><img src="<?=$this->config->base_url()?>/static/images/s-01.jpg" alt="相册图片-示例图片（1）"/></a></li>
+                        <li class="album-carousel-thumb li2"><a href="<?=$this->config->base_url()?>/static/images/02.jpg"><img src="<?=$this->config->base_url()?>/static/images/s-02.jpg" alt="相册图片-示例图片（2）"/></a></li>
+                        <li class="album-carousel-thumb li3"><a href="<?=$this->config->base_url()?>/static/images/03.jpg"><img src="<?=$this->config->base_url()?>/static/images/s-03.jpg" alt="相册图片-示例图片（3）"/></a></li>
+                        <li class="album-carousel-thumb li4"><a href="<?=$this->config->base_url()?>/static/images/04.jpg"><img src="<?=$this->config->base_url()?>/static/images/s-04.jpg" alt="相册图片-示例图片（4）"/></a></li>
+                        <li class="album-carousel-thumb li5"><a href="<?=$this->config->base_url()?>/static/images/05.jpg"><img src="<?=$this->config->base_url()?>/static/images/s-05.jpg" alt="相册图片-示例图片（5）"/></a></li>
+                        <!--下面li是保留一个空位置-->
+                        <li class="album-carousel-thumb"></li>
+                    </ul>
+                </div>
+                <!--<a href="#next-group" class="album-carousel-btn-next" id="album-carousel-btn-next">›</a>-->
+            </div>
+        </div>
+        <a href="#" class="btn_upload"></a>
+    </div>
+    <!--album end-->
+    <!--map start-->
+    <div class="map"></div>
+    <!--map end-->
+    <!--redian start-->
+    <div class="redian">
+        <div id="focus">
+
+            <a href="#"><img alt="戴佩妮独立作" src="<?=$this->config->base_url()?>/static/images/05.jpg"  width="415" height="258"/><s></s><p><span class="text">戴佩妮独立作</span><span class="play" ></span></p></a>
+            <a href="#"><img alt="专辑推荐：《中国最强音第八场》" src="<?=$this->config->base_url()?>/static/images/04.jpg"  width="415" height="258"/><s></s><p><span class="text">专辑推荐：《中国最强音第八场》</span><span class="play" ></span></p></a>
+            <a href="#"><img alt="戴佩妮独立作" src="<?=$this->config->base_url()?>/static/images/05.jpg"  width="415" height="258"/><s></s><p><span class="text">戴佩妮独立作</span><span class="play" ></span></p></a>
+            <a href="#"><img alt="专辑推荐：《中国最强音第八场》" src="<?=$this->config->base_url()?>/static/images/04.jpg"  width="415" height="258"/><s></s><p><span class="text">专辑推荐：《中国最强音第八场》</span><span class="play" ></span></p></a>
+            <a href="#"><img alt="戴佩妮独立作" src="<?=$this->config->base_url()?>/static/images/05.jpg"  width="415" height="258"/><s></s><p><span class="text">戴佩妮独立作</span><span class="play" ></span></p></a>
+            <a href="#"><img alt="专辑推荐：《中国最强音第八场》" src="<?=$this->config->base_url()?>/static/images/04.jpg"  width="415" height="258"/><s></s><p><span class="text">专辑推荐：《中国最强音第八场》</span><span class="play" ></span></p></a>
+            <a href="#"><img alt="戴佩妮独立作" src="<?=$this->config->base_url()?>/static/images/05.jpg"  width="415" height="258"/><s></s><p><span class="text">戴佩妮独立作</span><span class="play" ></span></p></a>
+            <a href="#"><img alt="专辑推荐：《中国最强音第八场》" src="<?=$this->config->base_url()?>/static/images/04.jpg"  width="415" height="258"/><s></s><p><span class="text">专辑推荐：《中国最强音第八场》</span><span class="play" ></span></p></a>
+
+            <div id="ctr">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!--redian end-->
+    <!--jiangpin start-->
+    <div class="jiangpin">
+        <div class="jpbox"><img src="<?=$this->config->base_url()?>/static/images/img_jp1.png"/></div>
+        <div class="btn_go"><a href="#" title="前往旅游局"></a></div>
+    </div>
+    <!--jiangpin end-->
+</div>
+<script type="text/javascript" src="<?=$this->config->base_url()?>/static/javascript/jquery.js"></script>
+<script type="text/javascript" src="<?=$this->config->base_url()?>/static/javascript/carousel.js"></script>
+<script type="text/javascript" src="<?=$this->config->base_url()?>/static/javascript/album.js"></script>
+<script type="text/javascript">
+    var Album = new jQuery.Album();
+</script>
+<script type="text/javascript" src="<?=$this->config->base_url()?>/static/javascript/base-min.js"></script>
+<script type="text/javascript" src="<?=$this->config->base_url()?>/static/javascript/Tab-min.js"></script>
+<script type="text/javascript">
+    t1 = new Tab("ctr", "span", "focus", "a", {
+        autoPlay : true,
+        timeout : 5000,
+        delay:200,
+        event : "mouseover"
+    });
+</script>
+
+</body>
+</html>
---
+++ static/javascript/Tab-min.js
@@ -0,0 +1,155 @@
+﻿function Tab(tabId, tabTag, contentId, contentTag, option) {
+    this.setOption(option);
+    var tabEl = Kg.$(tabId),
+    tabChild = tabEl.childNodes,
+    contentEl = Kg.$(contentId),
+    contentChild = contentEl.childNodes;
+    this.tabsFather = tabEl;
+    this.contentFather = contentEl;
+    this.tabs = [];
+    this.contents = [];
+    this.timer = null;
+    for (var i = 0,
+    l = tabChild.length; i < l; i++) {
+        if (tabChild[i].nodeType == 1 && tabChild[i].tagName.toLowerCase() == tabTag) {
+            this.tabs.push(tabChild[i])
+        }
+    }
+    for (var i = 0,
+    l = contentChild.length; i < l; i++) {
+        if (contentChild[i].nodeType == 1 && contentChild[i].tagName.toLowerCase() == contentTag) {
+            this.contents.push(contentChild[i])
+        }
+    }
+    this.currentIndex = this.option.index === "random" ? parseInt(Math.random() * this.tabs.length) : this.option.index;
+    for (var i = 0,
+    l = this.contents.length; i < l; i++) {
+        this.contents[i].style.display = "none"
+    }
+    this.tabs[this.currentIndex].className += " " + this.option.current;
+    this.contents[this.currentIndex].style.display = "block";
+    this.run()
+};
+Tab.prototype.setOption = function(option) {
+    this.option = {
+        event: "click",
+        effect: null,
+        fadeStep: 1,
+        current: "current",
+        loop: true,
+        index: 0,
+        autoPlay: false,
+        timeout: 1500,
+        delay: 0,
+        callback: null
+    };
+    Kg.extend(this.option, option || {},
+    true)
+};
+Tab.prototype.run = function() {
+    var timer = null;
+    for (var i = 0,
+    l = this.tabs.length; i < l; i++) {
+        Kg.addEvent(this.tabs[i], this.option.event, Kg.bind(function(index) {
+            if (this.option.event == "mouseover" && this.option.delay > 0) {
+                var _this = this;
+                timer = setTimeout(function() {
+                    _this.change(index)
+                },
+                this.option.delay)
+            } else {
+                this.change(index)
+            }
+        },
+        this, i));
+        if (this.option.event == "mouseover" && this.option.delay > 0) {
+            Kg.addEvent(this.tabs[i], "mouseout",
+            function() {
+                clearTimeout(timer)
+            })
+        }
+    }
+    if (this.option.autoPlay) {
+        this.auto();
+        Kg.addEvent(this.tabsFather, "mouseover", Kg.bind(function() {
+            clearInterval(this.timer)
+        },
+        this));
+        Kg.addEvent(this.contentFather, "mouseover", Kg.bind(function() {
+            clearInterval(this.timer)
+        },
+        this));
+        Kg.addEvent(this.tabsFather, "mouseout", Kg.bind(function() {
+            this.auto()
+        },
+        this));
+        Kg.addEvent(this.contentFather, "mouseout", Kg.bind(function() {
+            this.auto()
+        },
+        this))
+    }
+};
+Tab.prototype.change = function(index) {
+    var current = this.option.current;
+    var curIndex = this.currentIndex;
+    var lastCon = this.contents[curIndex];
+    var curCon = this.contents[index];
+    this.tabs[curIndex].className = this.tabs[curIndex].className.replace(new RegExp("\\s*" + current, "g"), "");
+    this.tabs[index].className += " " + current;
+    lastCon.style.display = "none";
+    curCon.style.display = "block";
+    if (this.option.effect === "fade") {
+        clearInterval(lastCon.timer);
+        Kg.setOpacity(this.contents[index], 0);
+        curCon.timer = Kg.fadein(curCon, 1, this.option.fadeStep)
+    }
+    this.currentIndex = index;
+    typeof(this.option.callback) == "function" && this.option.callback()
+};
+Tab.prototype.auto = function() {
+    clearInterval(this.timer);
+    this.timer = setInterval(Kg.bind(function() {
+        this.next()
+    },
+    this), this.option.timeout)
+};
+Tab.prototype.next = function() {
+    var index = (this.currentIndex + 1) > (this.tabs.length - 1) ? 0 : (this.currentIndex + 1);
+    if (this.option.loop === false && index === 0) {
+        return
+    }
+    this.change(index)
+};
+Tab.prototype.prev = function() {
+    var index = (this.currentIndex - 1) >= 0 ? (this.currentIndex - 1) : (this.tabs.length - 1);
+    if (this.option.loop === false && index === this.tabs.length - 1) {
+        return
+    }
+    this.change(index)
+};
+Tab.rotation = function(contentId, contentTag, timeout) {
+    var timer = null;
+    var el = Kg.$(contentId);
+    var els = Kg.$T(contentTag, el);
+    var index = 0;
+    Kg.addEvent(el, "mouseover",
+    function() {
+        clearInterval(timer)
+    });
+    Kg.addEvent(el, "mouseout",
+    function() {
+        go()
+    });
+    function go() {
+        timer = window.setInterval(function() {
+            for (var i = 0; i < els.length; i++) {
+                if (els[i].offsetHeight > 0) index = i;
+                els[i].style.display = "none"
+            }
+            var num = (index >= els.length - 1) ? 0 : index + 1;
+            els[num].style.display = "block"
+        },
+        timeout)
+    }
+    go()
+}
